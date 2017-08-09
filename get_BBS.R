38c9006573d89e0642107f7f602b31d5985e5ce1
Clark <crushing05@gmail.com>
2017-08-09 09:22:10 -0400

    Update version to 2016.1.0

--- get_BBS.R
+++ get_BBS.R
@@ -1,12 +1,16 @@
+# httr::set_config(httr::config(ssl_verifypeer = 0L))
 # devtools::install_github('crushing05/rBBS')
 # library(rBBS)
 
-counts <- GetRouteData(TenStops = FALSE)
-weather <- GetWeather()
-routes <- GetRoutes()
+counts <- rBBS::GetRouteData(TenStops = FALSE)
+weather <- rBBS::GetWeather()
+routes <- rBBS::GetRoutes()
 
 bbs <- list(counts = counts, weather = weather, routes = routes)
 saveRDS(bbs, 'bbs_fiftystop.rds')
+
+httr::set_config(httr::config(ssl_verifypeer = 0L))
+BBS.fiftystop::bbs50_release()
 
 pkg <- datastorr::autogenerate("crushing05/BBS.fiftystop", "readRDS",filename = "bbs_fiftystop.rds",
                         name="bbs_fiftystop", roxygen=TRUE)
